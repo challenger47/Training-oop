b44461bd3d713aac1e41f577c0c89ddaed08d9f4
Ersin <ersinakyuz47@gmail.com>
2021-02-03 23:53:49 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService //tüm interface lere Service yaz
+    {
+        List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int id);
+        List<Product> GetByUnitPrice(decimal min, decimal max);
+    }
+}
